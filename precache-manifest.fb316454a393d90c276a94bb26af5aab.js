c394135179ce538d6d2591b1d10817f558c25779
UntilTheCore <814898862@qq.com>
2020-08-11 19:29:14 +0800

    deploy

---
+++ precache-manifest.fb316454a393d90c276a94bb26af5aab.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bedebcdf79a03c914d1",
+    "url": "/fingertips-books-website/css/app.6c422d83.css"
+  },
+  {
+    "revision": "03e74e907e0977578654",
+    "url": "/fingertips-books-website/css/chunk-25452581.db4e7913.css"
+  },
+  {
+    "revision": "26ef0d6c0c0177eb7199",
+    "url": "/fingertips-books-website/css/chunk-3ad884a6.078b2bef.css"
+  },
+  {
+    "revision": "9849b9a73fb8d37d9495",
+    "url": "/fingertips-books-website/css/chunk-613c247a.c3f54dde.css"
+  },
+  {
+    "revision": "02e7752a57e1b8400d3f",
+    "url": "/fingertips-books-website/css/chunk-73782fe4.5703bc0b.css"
+  },
+  {
+    "revision": "ba2a2c286d451a6dea68d41be5283e9e",
+    "url": "/fingertips-books-website/index.html"
+  },
+  {
+    "revision": "6bedebcdf79a03c914d1",
+    "url": "/fingertips-books-website/js/app.ceb1dc48.js"
+  },
+  {
+    "revision": "03e74e907e0977578654",
+    "url": "/fingertips-books-website/js/chunk-25452581.3211787f.js"
+  },
+  {
+    "revision": "663ac88cd10793898512",
+    "url": "/fingertips-books-website/js/chunk-2d0e5ad4.6bd6c0e0.js"
+  },
+  {
+    "revision": "26ef0d6c0c0177eb7199",
+    "url": "/fingertips-books-website/js/chunk-3ad884a6.d4a2fbac.js"
+  },
+  {
+    "revision": "9849b9a73fb8d37d9495",
+    "url": "/fingertips-books-website/js/chunk-613c247a.1f477441.js"
+  },
+  {
+    "revision": "02e7752a57e1b8400d3f",
+    "url": "/fingertips-books-website/js/chunk-73782fe4.0ae63e6b.js"
+  },
+  {
+    "revision": "0ea90fda8c65dce9c578",
+    "url": "/fingertips-books-website/js/chunk-vendors.16f33109.js"
+  },
+  {
+    "revision": "d2b1ff142668165e79fa58e7d1429c9e",
+    "url": "/fingertips-books-website/manifest.json"
+  }
+]);
